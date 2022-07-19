b4549332973fde7a1d6abc90f19d29c16db36036
Nathanael Tjahyadi <56219164+Nathanael1721@users.noreply.github.com>
2022-07-19 16:49:55 +0700

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,3 +1,6 @@
+from traceback import print_tb
+
+
 def menu():
     import os
     os.system("cls")
@@ -9,7 +12,8 @@ def menu():
     print("3. Mencari Data")
     print("4. Mengubah Data")
     print("5. Menghapus Data")
-    print("6. Keluar")
+    print("6. Urutkan Data")    
+    print("7. Keluar")
     print("===================================")
     
     no = int(input("\nMasukan Menu Yang Anda Inginkan : "))
@@ -27,13 +31,15 @@ def no_menu(a):
     elif a == 5:
         hapus_data()
     elif a == 6:
-        keluar_program()
+        urutkan_data()
+    elif a == 7:
+        keluar_program()    
 #================================================#
 def lihat_data():
     print("\n================================")
     print("Anda Berada Pada Menu Menampilkan Data ")
     print("================================")
-    f = open("Tugas Akhir\data.txt")
+    f = open("data.txt")
     isi = f.readlines()
     isi.sort
     if len(isi) == 0 :
@@ -43,9 +49,12 @@ def lihat_data():
         for x in isi:
             pisah = x.split(",")
             print("Data", str(i))
-            print("Nama : "+pisah[0])
-            print("Umur : "+pisah[1],"Tahun")
-            print("Asal : "+pisah[2])
+            print("Nama                : "+pisah[0])
+            print("Umur                : "+pisah[1],"Tahun")
+            print("Asal                : "+pisah[2])
+            print("Agama               : "+pisah[3])
+            print("Pendidikan Terakhir : "+pisah[4])
+
             i +=1
     print("Tekan [Enter] Untuk Melanjutkan")
     f.close()
@@ -58,11 +67,13 @@ def tambahkan_data():
     print("===============================")
     print("\nMasukkan Data Anda : ")
     print("_______________________")
-    n = input("Nama : ")
-    u = input("Umur : ")
-    a = input("Asal : ")
-    f = open("Tugas Akhir\data.txt", "a")
-    f.writelines([n+","+u+","+a+"\n"])
+    n = input("Nama                : ")
+    u = input("Umur                : ")
+    a = input("Asal                : ")
+    b = input("Agama               : ")
+    c = input("Pendidikan Terakhir : ")
+    f = open("data.txt", "a")
+    f.writelines([n+","+u+","+a+","+b+","+c+"\n"])
     print("\nTekan [Enter] Untuk Melanjutkan")
     f.close()
     input()
@@ -72,20 +83,22 @@ def cari_data():
     print("\n==================================")
     print("Anda Berada Pada Menu Mencari Data")
     print("==================================")
-    f = open("Tugas Akhir\data.txt")
+    f = open("data.txt")
     nc = input("Masukkan Nama Yang Ingin Anda Cari : ")
     isi = f.readlines()
     
     idx = 0
-    urut = 1
+    #urut = 12
     for a in isi:
 #        print(a)
         x = a.split(",")
         if nc in x :
             print("Data Ditemukan !!!")
-            print("Nama : "+x[0])
-            print("Usia : "+x[1])
-            print("Asal : "+x[2])
+            print("Nama                : "+x[0])
+            print("Usia                : "+x[1])
+            print("Asal                : "+x[2])
+            print("Agama               : "+x[3])
+            print("Pendidikan Terakhir : "+x[4])
         idx += 1
         # if x[0] == nc:
         #     print("\nData Ditemukan, Data Ke : ", urut)
@@ -95,7 +108,7 @@ def cari_data():
         #     print("Asal : "+x[2])
         #     urut += 1
         #     continue
-        #     break
+        #     breakasd asdas asd
         # idx += 1
         # if idx == len(isi):
         #     print("DATA TIDAK DITEMUKAN !!!")
@@ -111,11 +124,13 @@ def update_data():
     upnama = input("Masukkan Nama Data Yang Ingin Diubah : ")
     print("\nMasukkan Data Baru !!")
     print("_______________________")
-    nb = input("Masukkan Nama Baru : ")
-    ub = input("Masukkan Umur Baru : ")
-    ab = input("Masukkan Asal Baru : ")
+    nb = input("Masukkan Nama Baru             : ")
+    ub = input("Masukkan Umur Baru             : ")
+    ab = input("Masukkan Asal Baru             : ")
+    bb = input("Masukkan Agama Baru            : ")
+    cb = input("Masukkan Pendidikan Akhir Baru : ")
     
-    f = open("Tugas Akhir\data.txt")
+    f = open("data.txt")
     isi = f.readlines()
     idx = 0
     for x in isi :
@@ -123,14 +138,16 @@ def update_data():
         if data[0] == upnama:
             data[0] = nb
             data[1] = ub
-            data[2] = ab + "\n"
+            data[2] = ab 
+            data[3] = bb
+            data[4] = cb + "\n"
             datax = ",".join(data)
             isi[idx] = datax
             break
         idx += 1
     f.close()
     
-    f = open("Tugas Akhir\data.txt","w")
+    f = open("data.txt","w")
     isi = f.writelines(isi)
     
     print("\nTekan [Enter] Untuk Melanjutkan")
@@ -158,7 +175,7 @@ def hapus_data():
         idx += 1
     f.close()
     
-    f = open("Tugas Akhir\data.txt","w")
+    f = open("data.txt","w")
     isi = f.writelines(isi)
     
     print("\nTekan [Enter] Untuk Melanjutkan")
@@ -166,6 +183,23 @@ def hapus_data():
     input()
     menu()
 #=======================================================#
+
+def urutkan_data():
+    f = open("data.txt","r")
+    isi = f.readlines()
+    isi.sort()
+    for a in isi:
+        x = a.split(",")
+        print("Nama                : "+x[0])
+        print("Usia                : "+x[1])
+        print("Asal                : "+x[2])
+        print("Agama               : "+x[3])
+        print("Pendidikan Terakhir : "+x[4])
+    print("\nTekan [Enter] Untuk Melanjutkan")
+    f.close()
+    input()
+    menu()
+
 def keluar_program():
     print("\nTekan [Enter] Untuk Melanjutkan")
 
